cd343270c9be490075f14de49e4c7b8b67ef6b56
Orest <Orestduda@gmail.com>
2021-04-23 13:58:35 +0300

    fix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,7 +5,7 @@ import styles from './Statistics.module.css';
 const Statistics = ({title, stats}) => {
     return (
         <section className={styles.statistics}>
-            {title === '' ? false : <h2 className={styles.title}>{title}</h2>}
+            {title && <h2 className={styles.title}>{title}</h2>}
             <ul className={styles.statList}>
                 {stats.map((stat)=>(
                     <li className={styles.item}
